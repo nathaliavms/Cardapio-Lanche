4df74a943c46c25550590578cde1502135cc5fa8
Nathalia Silva <Silva.Nathalia@ibm.com>
2022-08-11 17:35:05 -0300

    first commit

---
+++ Desafio/Cardapio.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio
+{
+    class Cardapio
+    {
+        public int Codigo;
+        public int Quantidade;
+
+        public double MultiplicarDoisValores()
+        {
+            if (Codigo == 1)
+
+                return Quantidade * 4.00;
+
+            else if (Codigo == 2)
+
+                return Quantidade * 4.50;
+
+            else if (Codigo == 3)
+
+                return Quantidade * 5.00;
+
+            else if (Codigo == 4)
+
+                return Quantidade * 2.00;
+
+            else
+
+                return Quantidade * 1.50;
+
+        }
+    }
+}
---
+++ Desafio/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cardapio Produto = new Cardapio();
+
+            Console.WriteLine("Digite o Código do produto desejado e a quantidade (na mesma linha):");
+            string[] Numero = Console.ReadLine().Split();
+            Produto.Codigo = int.Parse(Numero[0]);
+            Produto.Quantidade = int.Parse(Numero[1]);
+
+            Console.WriteLine("Total: R$ " + Produto.MultiplicarDoisValores().ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
